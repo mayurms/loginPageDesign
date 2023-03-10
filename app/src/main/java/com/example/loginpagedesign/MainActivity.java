5220a17db53f5cb9f7593bae6d40faa059fc2198
mayur <mayurms5060@gmail.com>
2023-03-10 13:08:38 +0530

    first commit

---
+++ app/src/main/java/com/example/loginpagedesign/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.loginpagedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText,passwordText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        editText = findViewById(R.id.edittext);
+        passwordText = findViewById(R.id.passwordText);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
